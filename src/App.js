dccd3df97cd8fbad94633a4ea5f06a1ebff374a9
Veronica Nerak <veronicanerak@gmail.com>
2019-12-04 11:33:20 +0100

    Components, propsTyupes and defaultProps examples added.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,36 @@ import React, {Fragment} from 'react';
 import "./styles/styles.scss"
 import Curso from "./Curso"
 
+
+const cursos = [
+  {
+    "title": "React desde cero",
+    "image": "https://wallpapercave.com/wp/Q89pEav.jpg",
+    "price": 30,
+    "profesor": "Beto Quiroga"
+  }, 
+  {
+    "title": "HTML desde cero",
+    "image": "https://marketingweek.imgix.net/content/uploads/2018/11/23153217/christmas.png",
+    "price": 50,
+    "profesor": "Josue Martinez"
+  }, 
+  {
+    "title": "Drupal desde cero",
+    "image": "https://image.shutterstock.com/image-photo/decorated-christmas-tree-on-blurred-260nw-1201088539.jpg",
+    "price": 10,
+    "profesor": "Alexyis Lozada"
+  },
+  {
+    "title": "CSS desde cero",
+    "image": "https://cdn.pixabay.com/photo/2014/12/21/07/50/christmas-card-574742__340.jpg",
+    "price": 60,
+    "profesor": "Alvaro Felipe"
+  }
+]
+
+
+
 const App = () => (
 <>
 <div className="main-banner img-container l-section" id="main-banner">
@@ -17,21 +47,10 @@ const App = () => (
   </div>
 </div>
 
-<div className="ed-grid m-grid-3">
-  <Curso />
-  <Curso />
-  <Curso />
-  <Curso />
-  <Curso />
-  <Curso />
-  <Curso />
-  <Curso />
-  <Curso />
-  <Curso />
-  <Curso />
-  <Curso />
-  <Curso />
-  <Curso />
+<div className="ed-grid m-grid-3">  
+  {
+    cursos.map( curso => <Curso title={curso.title} image={curso.image} price={curso.price} profesor={curso.profesor} /> )
+  }
 </div>
 </>
 )
--- src/Curso.jsx
+++ src/Curso.jsx
@@ -1,29 +1,44 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const Curso = () => (
+const curso = {
+  "title": "React desde cero",
+  "image": "https://wallpapercave.com/wp/Q89pEav.jpg",
+  "price": "50usd"
+}
+
+const Curso = ({title, image, price, profesor}) => (
   <article class="card">
-    <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-      <img src="https://wallpapercave.com/wp/Q89pEav.jpg" alt="test" />
+    <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">      
+      <img src={image} alt={ title } />
     </div>
     <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
-      <h3 className="t5 s-mb-2 s-center">
-        Programación orientada a objetos con Go
-      </h3>
-      <div className="s-mb-2 s-main-center">
-        <div className="card__teacher s-cross-center">
-          <div className="card__avatar s-mr-1">
-            <div className="circle img-container">
-              <img src="https://images.unsplash.com/photo-1516571934337-d366d0c75b89?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" alt="" />
-            </div>
-          </div>
-          <span className="small">Alexys Lozada</span>
-        </div>
-      </div>
+      <h3 className="t5 s-mb-2 s-center">       
+        { title }       
+      </h3> 
+      <div>
+        { `El profesor es: ${profesor}` }
+      </div>     
       <div className="s-main-center">
-        <a className="button--ghost-alert button--tiny" href="#">$ 20USD</a>
+        <a className="button--ghost-alert button--tiny" href="#">{ `$ ${price} USD` }</a>
       </div>
     </div>
   </article>
 )
+
+Curso.propTypes = {
+  title: PropTypes.string,
+  image: PropTypes.string,
+  price: PropTypes.number,
+  profesor: PropTypes.string
+}
+
+Curso.defaultProps = {
+  title: "No se encontro titulo",
+  image: "https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX3080623.jpg",
+  price: "--",
+  profesor: ""
+}
+
 
 export default Curso
