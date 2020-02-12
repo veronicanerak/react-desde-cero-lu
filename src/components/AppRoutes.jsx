b749a6825e1b77b137f19bbb85b9ee43cadbbac3
Veronica Nerak <veronicanerak@gmail.com>
2020-02-12 19:18:02 +0100

    AppRoutes component has been created with all the routes for the main menu.

---
+++ src/components/AppRoutes.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Form from "./Pages/Form";
+import Courses from './Pages/Courses';
+import CourseFullView from "./Pages/CourseFullView"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import MainMenu from './Organisms/MainMenu'
+import HistoryPage from './Pages/HistoryPage';
+import Home from './Pages/Home';
+import Users from './Pages/Users';
+import CustomHooksExample from './Pages/CustomHooksExample';
+
+const AppRoutes = () => (
+    <Router>
+        <MainMenu />
+        <Switch>
+        <Route path="/" exact component= { Home} />
+        <Route path="/courses/:course_id" component={ CourseFullView } />
+        <Route path="/courses" component= { Courses } />
+        <Route path="/history/:history_id" component= { HistoryPage } />
+        <Route path="/history" component= { HistoryPage } />
+        <Route path="/users" component= { Users } />
+        <Route path="/contact" component= { () => <Form form_name="Contact Us" /> } />
+        <Route path="/custom-hooks" component= { CustomHooksExample } />
+        <Route component={()=> (
+            <div className="ed-grid">
+            <h1>Error 404</h1>
+            <span>Page not found</span>
+            </div>
+        )} />
+        </Switch>
+    </Router>
+)
+
+export default AppRoutes
